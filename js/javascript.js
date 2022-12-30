bc663162da6e686b2132049edc26babf96e9d76a
Alexis Tejada <tejada.alexis.m@gmail.com>
2022-12-30 10:16:41 -0300

    arreglo

--- js/javascript.js
+++ js/javascript.js
@@ -6,18 +6,18 @@ function sumar(prod1,prod2,prod3){
     return prod1 + prod2 + prod3
 }
 
-let primerProd = Number(prompt("Ingresa el primer precio "))
-let segundoProd = Number(prompt("ingresa el segundo precio "))
-let tercerProd = Number(prompt("ingresa el tercer precio "))
+let primerProd = Number(prompt("Ingresa el valor del primer item "))
+let segundoProd = Number(prompt("ingresa el valor del segundo item "))
+let tercerProd = Number(prompt("ingresa el valor del tercer item "))
 
 let PFApagar = sumar(primerProd,segundoProd,tercerProd)
 
-var descuento = PFApagar*0.85
+var descuento = PFApagar*0.80
 
 let pagoEfectivo = prompt("abona en efectivo? ")
 
-if (pagoEfectivo = "si,Si,SI,s,S") {
-    alert("ha obtenido un descuento del 15% su monto a abonar es: " +(descuento), )
+if (pagoEfectivo ==="si" || pagoEfectivo === "Si" || pagoEfectivo === "SI" || pagoEfectivo === "sI") {
+    alert("ha obtenido un descuento del 20% su monto a abonar es: " +(descuento), )
 } 
 else {
     alert("su monto a abonar es: " + (PFApagar), )
